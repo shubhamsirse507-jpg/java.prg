a8182a42d7c9b2c90de647b581b565496c8dc547
shubhamsirse507-jpg <shubhamsirse507@gmail.com>
2025-12-09 20:16:10 +0530

    Add files via upload

---
+++ code4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class code4{
+    public code4(){
+
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner (System.in);
+
+     //float a = 7/4.0f* 9/2.0f;
+     //System.out.println(a);
+     //type casting
+     char grade='b';
+     grade =(char)(grade + 8);
+     System.out.println(grade);
+    //  decrease the grade by 8
+     grade =(char)(grade - 8);
+     System.out.println(grade);
+    }
+}
