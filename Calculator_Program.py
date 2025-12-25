0041e647878ebe6fa51e2c4d0dc9ace0f7fa3b84
shubhamsirse507-jpg <shubhamsirse507@gmail.com>
2025-12-25 23:21:52 +0530

    Add files via upload

---
+++ Calculator_Program.py
@@ -0,0 +1,10 @@
+a =int(input("Enter first number:"))
+b =int(input("Enter second number:"))
+c = a+b , a-b , a*b , a/b , a%b
+print("Sum is:",c)
+
+if(a+b,a-b,a*b,a/b,a%b>50):
+    print("All results are grater than 50",c)
+else:
+    print("All results are less than 50:)")
+#this is Simple Calculator Program
